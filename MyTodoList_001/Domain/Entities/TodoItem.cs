687f3f0bce79868661fa5033b2d9ad637f15959b
User <User@DESKTOP-QQHDURR>
2024-07-26 10:02:26 +0700

    Add project files.

---
+++ MyTodoList_001/Domain/Entities/TodoItem.cs
@@ -0,0 +1,11 @@
+﻿namespace MyTodoList_001.Domain.Entities
+{
+    public class TodoItem
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Description { get; set; } = "";
+        public DateOnly DueDate { get; set; }
+        public DateTime ReminderTime { get; set; }
+    }
+}
